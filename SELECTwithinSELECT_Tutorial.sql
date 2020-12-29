f0fc91769b39f0a14b7045cfb0d5128436b70a5a
Amanda Gomez <o0amandagomez0o@gmail.com>
2020-12-28 22:03:17 -0600

    completed problems 8-10

--- SELECTwithinSELECT_Tutorial.sql
+++ SELECTwithinSELECT_Tutorial.sql
@@ -116,10 +116,39 @@ WHERE x.name <= ALL(
                     WHERE x.continent=y.continent)
 ORDER BY continent
 
+/*
+Difficult Questions That Utilize Techniques Not Covered In Prior Sections
+9.
+Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.
+*/
+SELECT name, continent, population
+FROM world x
+WHERE 25000000 >= ALL(SELECT population
+                    FROM world y
+                    WHERE x.continent = y.continent
+                    AND population > 0)
+/*
+10.
+Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
+*/
+SELECT name, continent
+FROM world x
+WHERE 3*(population) <= ALL(SELECT population
+                        FROM world y 
+                        WHERE x.continent = y.continent
+                        AND 
+                        population > 0
+                        AND 
+                        y.continent = y.continent)
 
+SELECT name, continent
+FROM world x
+WHERE population > ALL(SELECT population *3
+                        FROM world y 
+                        WHERE x.continent = y.continent
+                        AND 
+                        population > 0
+                        AND 
+                        y.name <> x.name)
 
-
-
-
-
-
+--need clarification on why reversing inequalty is necessary
